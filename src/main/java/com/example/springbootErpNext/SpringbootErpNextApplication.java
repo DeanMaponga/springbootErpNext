d2d5c7955204cd5e8b063f445e0cc3646a3df06e
Dean Maponga <103897582+DeanMaponga@users.noreply.github.com>
2023-10-10 15:25:48 +0200

    Initial run

---
+++ src/main/java/com/example/springbootErpNext/SpringbootErpNextApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootErpNext;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootErpNextApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootErpNextApplication.class, args);
+	}
+
+}
