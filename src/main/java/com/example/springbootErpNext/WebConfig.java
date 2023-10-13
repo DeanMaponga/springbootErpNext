82afbffd197be5d511a6121e69e23d9827a3f6e9
Dean Maponga <103897582+DeanMaponga@users.noreply.github.com>
2023-10-13 10:26:20 +0200

    apikeys setup

--- src/main/java/com/example/springbootErpNext/WebConfig.java
+++ src/main/java/com/example/springbootErpNext/WebConfig.java
@@ -3,10 +3,11 @@ package com.example.springbootErpNext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class CorsConfig {
+public class WebConfig {
 
     @Bean
     public WebMvcConfigurer corsConfigurer() {
@@ -18,6 +19,11 @@ public class CorsConfig {
                         .allowedMethods("GET", "POST", "PUT", "DELETE")
                         .allowedHeaders("*")
                         .allowCredentials(true);
+            }
+
+             @Override
+            public void addInterceptors(InterceptorRegistry registry) {
+                registry.addInterceptor(new ApiTokenFilter());
             }
         };
     }
